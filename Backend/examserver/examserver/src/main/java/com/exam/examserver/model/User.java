45dddf3902bc8a7381f87a9316c68c4b5b05b96e
Tapan Kumar Barik <tapankumarbarik7@gmail.com>
2022-02-17 07:46:32 +0530

    created tables for user role and users

---
+++ Backend/examserver/examserver/src/main/java/com/exam/examserver/model/User.java
@@ -0,0 +1,164 @@
+package com.exam.examserver.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String userName;
+	
+	private String password;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String phoneNo;
+	
+	private String about;
+	
+	private String Profile;
+	
+	private boolean enabled=true;
+	
+	@OneToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER,mappedBy="user")
+	@JsonIgnore
+	private Set<UserRole>userRoles=new HashSet<>();
+	
+	
+	
+	
+	public User (){
+		
+	}
+	
+		
+	
+	
+
+	public User(Long id, String userName, String password, String firstName, String lastName, String email,
+			String phoneNo, String about, String profile, boolean enabled, Set<UserRole> userRoles) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNo = phoneNo;
+		this.about = about;
+		Profile = profile;
+		this.enabled = enabled;
+		this.userRoles = userRoles;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public String getProfile() {
+		return Profile;
+	}
+
+	public void setProfile(String profile) {
+		Profile = profile;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+	
+	
+	
+
+}
